ef736dfe52da90d3710b729970a5c875bbaee6cf
Peter Ovchinnikov <peter@devteamforyou.com>
2017-02-10 23:40:36 +0200

    Initial checkin

---
+++ ReactiveFire/ReactiveFire.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveFire.h
+//  ReactiveFire
+//
+//  Created by Peter Ovchinnikov on 2/10/17.
+//  Copyright © 2017 Crossroad Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ReactiveFire.
+FOUNDATION_EXPORT double ReactiveFireVersionNumber;
+
+//! Project version string for ReactiveFire.
+FOUNDATION_EXPORT const unsigned char ReactiveFireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveFire/PublicHeader.h>
+
+
